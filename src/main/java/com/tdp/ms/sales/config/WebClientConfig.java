3d892a784382b5fd18aad3ec59bc6bacee87e1d1
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-10-02 13:47:20 -0500

    fixing timout saleslead post

--- src/main/java/com/tdp/ms/sales/config/WebClientConfig.java
+++ src/main/java/com/tdp/ms/sales/config/WebClientConfig.java
@@ -1,14 +1,9 @@
 package com.tdp.ms.sales.config;
 
-import io.netty.channel.ChannelOption;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import javax.net.ssl.SSLException;
-
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -37,14 +32,6 @@ public class WebClientConfig {
 
     //@Value("${application.security.token.url}")
     private static String tokenUrl = "";
-
-    @Value("${application.endpoints.url.business_parameters.timeout.timeout}")
-    private String timeout;
-    @Value("${application.endpoints.url.business_parameters.timeout.timeout_read}")
-    private String readTimeout;
-    @Value("${application.endpoints.url.business_parameters.timeout.timeout_write}")
-    private String writeTimeout;
-
 
     /**
      * Bean to config Webclient.
@@ -84,15 +71,12 @@ public class WebClientConfig {
      */
     @Bean
     public WebClient webClientInsecure() throws SSLException {
-        SslContext sslContext = sslContext();
-        HttpClient httpClient =
-                httpClient(Integer.parseInt(timeout),
-                        Integer.parseInt(readTimeout),
-                        Integer.parseInt(writeTimeout))
-                        .secure(t -> t.sslContext(sslContext));
-
-        return createWebclient(tokenUrl,httpClient);
-
+        SslContext sslContext = SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
+        return WebClient.builder()
+                // .baseUrl(tokenUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient)).build();
     }
 
     /**
@@ -103,30 +87,6 @@ public class WebClientConfig {
     @Bean
     public WebClient webClientSecure() {
         return WebClient.builder().build();
-    }
-
-    private HttpClient httpClient(int connectionTimeOut, int readTimeout, int writeTimeout) {
-        return HttpClient.create()
-                .tcpConfiguration(client ->
-                        client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionTimeOut)
-                                .doOnConnected(conn -> conn
-                                        .addHandlerLast(new ReadTimeoutHandler(readTimeout / 1000))
-                                        .addHandlerLast(new WriteTimeoutHandler(writeTimeout / 1000))));
-    }
-
-    private SslContext sslContext() throws SSLException {
-        return SslContextBuilder
-                .forClient()
-                .trustManager(InsecureTrustManagerFactory.INSTANCE)
-                .build();
-    }
-
-    private WebClient createWebclient(String url, HttpClient httpClient) {
-        return WebClient
-                .builder()
-                .baseUrl(url)
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
     }
 
 }
