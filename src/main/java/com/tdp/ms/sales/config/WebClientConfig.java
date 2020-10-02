9afdcfee98dadc184e34b6947cd3e720234bdbca
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-10-02 14:34:19 -0500

    fixing PMD

--- src/main/java/com/tdp/ms/sales/config/WebClientConfig.java
+++ src/main/java/com/tdp/ms/sales/config/WebClientConfig.java
@@ -1,14 +1,10 @@
 package com.tdp.ms.sales.config;
 
-import io.netty.channel.ChannelOption;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import javax.net.ssl.SSLException;
 
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
