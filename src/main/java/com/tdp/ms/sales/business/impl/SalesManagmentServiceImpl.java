7e8a0af638e257a8a937ad7299734d9531a1d29a
Sergio Rivas <srivasme@everis.com>
2021-01-28 15:06:40 -0500

    R1-FEAT1208-CR-FEMSXXXX: MS Quotation Management - Cambio de variables para creación de financiamiento (CR)

--- src/main/java/com/tdp/ms/sales/business/impl/SalesManagmentServiceImpl.java
+++ src/main/java/com/tdp/ms/sales/business/impl/SalesManagmentServiceImpl.java
@@ -1244,9 +1244,9 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
                 .name(sale.getRelatedParty().get(0).getFirstName())
                 .surname(sale.getRelatedParty().get(0).getLastName())
                 .segment(this.getStringValueByKeyFromAdditionalDataList(sale.getAdditionalData(),
-                        "releatedPartySegment"))
+                        "customerTypeCode"))
                 .subsegment(this.getStringValueByKeyFromAdditionalDataList(sale.getAdditionalData(),
-                        "releatedPartySubSegment"))
+                        "customerSubTypeCode"))
                 .contactMedia(contactMediumList)
                 .legalId(legalId)
                 .address(address)
@@ -1292,7 +1292,7 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
 
         Channel channel = Channel
                 .builder()
-                .name(sale.getChannel().getName())
+                .name(sale.getChannel().getId())
                 .build();
 
         // Financiamiento de Instalación
@@ -1496,6 +1496,11 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
                 .key("reservationDate")
                 .value(Commons.getDatetimeNow())
                 .build();
+
+        if (saleRequest.getCommercialOperation().get(0).getDeviceOffering().get(0).getAdditionalData() == null) {
+            saleRequest.getCommercialOperation().get(0).getDeviceOffering().get(0).setAdditionalData(new ArrayList<>());
+        }
+
         saleRequest.getCommercialOperation().get(0).getDeviceOffering()
                 .get(0).getAdditionalData().add(dateKv);
 
@@ -1633,7 +1638,7 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
 
         MoneyAmount totalAmount = MoneyAmount
                 .builder()
-                .units("")
+                .units("PEN")
                 .amount(amountTotalAmount.toString())
                 .build();
 
@@ -1664,7 +1669,7 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
         Channel channel = Channel.builder().name(sale.getChannel().getName()).build();
 
         MoneyAmount totalCost = MoneyAmount
-                .builder().units("")
+                .builder().units("PEN")
                 .amount(sale.getCommercialOperation().get(0).getDeviceOffering().get(0).getOffers().get(0)
                         .getBillingOfferings().get(0).getCommitmentPeriods().get(0).getFinancingInstalments().get(0)
                         .getInstalments().getTotalAmount().getValue().toString()).build();
@@ -1673,7 +1678,7 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
                 .amount(sale.getCommercialOperation().get(0).getDeviceOffering().get(0).getOffers().get(0)
                         .getBillingOfferings().get(0).getCommitmentPeriods().get(0).getFinancingInstalments().get(0)
                         .getInstalments().getTotalAmount().getValue().toString())
-                .units("")
+                .units("PEN")
                 .build();
 
         List<com.tdp.ms.sales.model.dto.quotation.Item> itemsList = new ArrayList<>();
@@ -1692,7 +1697,7 @@ public class SalesManagmentServiceImpl implements SalesManagmentService {
                     .builder()
                     .amount(sale.getCommercialOperation().get(0).getDeviceOffering().get(1).getSimSpecifications()
                                                                         .get(0).getPrice().get(0).getValue().toString())
-                    .units("")
+                    .units("PEN")
                     .build();
 
             com.tdp.ms.sales.model.dto.quotation.Item itemSim = com.tdp.ms.sales.model.dto.quotation.Item
